cca4707438fc1fdfd84329c047c63060f258bc4f
parkikbum <pibum@naver.com>
2021-04-08 22:59:26 +0900

    [ADD] 과제 최종완료

--- Kakaotalk_Clone/Kakaotalk_Clone/AcoountViewController.swift
+++ Kakaotalk_Clone/Kakaotalk_Clone/AcoountViewController.swift
@@ -10,6 +10,7 @@ class AcoountViewController: UIViewController {
     @IBOutlet weak var EmailOrPhoneNumber: UITextField!
     @IBOutlet weak var PassWordInput: UITextField!
     @IBOutlet weak var PasswordCorrect: UITextField!
+    @IBOutlet weak var passwordNotCorrect: UILabel!
     var Email : String?
     var PassWord : String?
     var PassWordCor : String?
@@ -21,11 +22,20 @@ class AcoountViewController: UIViewController {
     @IBAction func CreateAccount(_ sender: Any) {
         if EmailOrPhoneNumber.text != "" && PassWordInput.text != "" && PasswordCorrect.text != ""
         {
+            if PassWordInput.text == PasswordCorrect.text{
         guard let nextCreate = self.storyboard?.instantiateViewController(identifier: "AccountCorrectViewController")
                 as? AccountCorrectViewController else{return}
         nextCreate.UsEmail = EmailOrPhoneNumber.text
         self.present(nextCreate, animated: true, completion: nil) //다음화면으로 넘어가기
         self.navigationController?.popViewController(animated: true) // 다음화면으로넘어가면서 pop시켜버리기
+            }
+            else {
+                passwordNotCorrect.text = "비밀번호가 일치하지 않습니다."
+            }
+    
     }
+        else{
+            passwordNotCorrect.text = "모두 입력해주세요."
+        }
     }
 }
